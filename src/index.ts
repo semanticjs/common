c52eb72e3fa6c67a7c303f6c1143f2f0e5a4163c
Michael Gearhardt <michael.gearhardt@gmail.com>
2021-08-05 19:04:40 -0600

    reorder

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,4 @@ import HTTPClient from "./api/HTTPClient";
 import Status from "./models/Status";
 import StringUtils from "./utils/StringUtils";
 
-export default { Status, HTTPClient, StringUtils };
+export default { StringUtils, Status, HTTPClient };
