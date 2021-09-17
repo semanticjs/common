f682b47c11e523a6ed2c7a56e6cfb5a712fb730e
mcgear <michael.gearhardt@gmail.com>
2021-09-17 12:00:29 -0600

    Latest

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@ import HTTPClient from './api/HTTPClient';
 import { DOMQuery } from './dom/DOMUtils';
 import { SJSDoc } from './dom/SJSDocument';
 import Status from './models/Status';
-// import { Capitalize} from './utils/StringUtils';
 import SJSElement from './elements/SJSElement';
 
 export { HTTPClient, Status, SJSElement, DOMQuery, SJSDoc };
