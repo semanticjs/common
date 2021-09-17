7159cc6f0b2d2ee628b14877168e21b6b73fb4de
mcgear <michael.gearhardt@gmail.com>
2021-09-17 09:44:24 -0600

    Fix build

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import HTTPClient, { client} from "./api/HTTPClient";
+import HTTPClient from "./api/HTTPClient";
 import Status from "./models/Status";
 import StringUtils from "./utils/StringUtils";
 import SJSElement from './elements/SJSElement'
