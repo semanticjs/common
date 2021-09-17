0678a251754444e2a414e2f1732ef0f88993c5dc
mcgear <michael.gearhardt@gmail.com>
2021-09-17 10:01:57 -0600

    Change to abstract and support for NodeListOf as array

--- src/elements/SJSElement.ts
+++ src/elements/SJSElement.ts
@@ -1,23 +1,39 @@
-export default class SJSElement {
-    //  Fields
-    protected mountedEl?: HTMLElement;
+export default abstract class SJSElement {
+  //  Fields
+  protected mountedEls: Array<HTMLElement>;
 
-    //  Properties
+  //  Properties
 
-    //  Constructors
-    constructor() {}
+  //  Constructors
+  constructor() {
+    this.mountedEls = [];
+  }
 
-    //  API Methods
-    public Mount(selector: string | HTMLElement): void {
-        if (typeof selector === 'string') {
-            selector = <HTMLElement>document.querySelector(<string>selector);
-        }
+  //  API Methods
+  public Mount(selector: string | HTMLElement | Array<Element>): void {
+    let elements: Array<HTMLElement> = [];
 
-        this.mountedEl = this.mountToElement(<HTMLElement>selector);
+    if (typeof selector === 'string') {
+      selector = Array.from(document.querySelectorAll(<string>selector));
     }
 
-    //  Helpers
-    protected mountToElement(el: HTMLElement): HTMLElement {
-        return el;
+    if (selector instanceof HTMLElement) {
+      elements = [selector];
+    } else {
+      const nodeList = Array.from(selector);
+
+      const htmlNodes = Array.from(nodeList)
+        .filter((node) => node instanceof HTMLElement)
+        .map((node) => <HTMLElement>node);
+
+      elements = [...htmlNodes];
     }
+
+    elements.forEach((el) => {
+      this.mountedEls.push(this.mountToElement(el));
+    });
+  }
+
+  //  Helpers
+  protected abstract mountToElement(el: HTMLElement): HTMLElement;
 }
