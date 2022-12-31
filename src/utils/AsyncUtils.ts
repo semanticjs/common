1cec5c9734796d99c1e0c9cac82faa68a4c789b1
mcgear <michael.gearhardt@gmail.com>
2022-12-31 07:18:16 -0700

    AsyncUtils

--- src/utils/AsyncUtils.ts
+++ src/utils/AsyncUtils.ts
@@ -1,6 +1,34 @@
 /*
  * Used to delay processing of something.
  */
-export function Delay(ms: number): Promise<unknown> {
-  return new Promise((resolve) => setTimeout(resolve, ms));
+export function Delay<TArgs extends any[]>(
+  ms: number,
+  ...args: TArgs
+): Promise<unknown> {
+  return new Promise((resolve) => setTimeout(resolve, ms, args));
 }
+
+declare global {
+  interface Promise<T> {
+    delay(ms: number): Promise<T>;
+  }
+}
+
+Promise.prototype.delay = function (ms: number): Promise<any> {
+  return this.then((args) => {
+    return Delay(ms, args);
+  });
+};
+
+declare global {
+  interface PromiseConstructor {
+    Delay(ms: number): Promise<unknown>;
+  }
+}
+
+Promise.Delay = function <TArgs extends any[]>(
+  ms: number,
+  ...args: TArgs
+): Promise<unknown> {
+  return Delay(ms, args);
+};
